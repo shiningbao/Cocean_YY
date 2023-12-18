caf2c6e5505bc7ee460f656da30fec150913bb32
JO-NoteBook\JO <scomjoo@gmail.com>
2023-12-18 09:54:21 +0900

    no message

---
+++ src/main/java/kr/co/cocean/CocoeanApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocoeanApplication.class, args);
+	}
+
+}
