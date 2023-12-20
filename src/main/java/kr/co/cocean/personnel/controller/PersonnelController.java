2c6d05f8a0ffb54756f332dc3040ba0266f52f44
gur2831 <gur2831@naver.com>
2023-12-20 17:47:16 +0900

    비밀번호수정

--- src/main/java/kr/co/cocean/personnel/controller/PersonnelController.java
+++ src/main/java/kr/co/cocean/personnel/controller/PersonnelController.java
@@ -59,7 +59,7 @@ public class PersonnelController {
 		ModelAndView mav = new ModelAndView("personnel/personnel");
 		logger.info("params =="+params);
 		String page= "";
-		String pw = "11";
+		String pw = "cocean1111";
 		
 		String password = encoder.encode(pw);
 		params.put("password", password);
