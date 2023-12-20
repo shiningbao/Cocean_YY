0d7aaa3e5b604ec059459dea35c572481dfdc5d3
JO-NoteBook\JO <scomjoo@gmail.com>
2023-12-20 09:16:19 +0900

    코션친구들 리스트, 등록 작성 중

---
+++ src/main/java/kr/co/cocean/aquarium/dto/ClassficationDTO.java
@@ -0,0 +1,38 @@
+package kr.co.cocean.aquarium.dto;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("classfication")
+public class ClassficationDTO {
+
+	private int speciesID;
+	private String taxonomy;
+	private String scientificName;
+	private String commonName;
+	
+	public int getSpeciesID() {
+		return speciesID;
+	}
+	public void setSpeciesID(int speciesID) {
+		this.speciesID = speciesID;
+	}
+	public String getTaxonomy() {
+		return taxonomy;
+	}
+	public void setTaxonomy(String taxonomy) {
+		this.taxonomy = taxonomy;
+	}
+	public String getScientificName() {
+		return scientificName;
+	}
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+	public String getCommonName() {
+		return commonName;
+	}
+	public void setCommonName(String commonName) {
+		this.commonName = commonName;
+	}
+	
+}
