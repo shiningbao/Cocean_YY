3d44a95386ca690b2b2c0c03574db950cd2582fe
shiningbao <147824819+shiningbao@users.noreply.github.com>
2023-12-20 10:39:05 +0900

    커밋

--- src/main/java/kr/co/cocean/approval/dto/ApprovalDTO.java
+++ src/main/java/kr/co/cocean/approval/dto/ApprovalDTO.java
@@ -8,14 +8,7 @@ public class ApprovalDTO {
 	private String titleID;
 	private String category;
 	private String title;
-	private int employeeID;
 	
-	public int getEmployeeID() {
-		return employeeID;
-	}
-	public void setEmployeeID(int employeeID) {
-		this.employeeID = employeeID;
-	}
 	public String getTitle() {
 		return title;
 	}
