4e101bc3857633ed91c16d7b6976359264695240
SCOM <scomjoo@gmail.com>
2024-01-04 15:11:55 +0900

    cocoean 삭제

--- src/main/java/kr/co/cocean/ServletInitializer.java
+++ src/main/java/kr/co/cocean/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CocoeanApplication.class);
+		return application.sources(CoceanApplication.class);
 	}
 
 }
